9ad6fcffb74737978ddd80b28683f19c77edf975
huixing <121674028@qq.com>
2018-03-26 15:06:33 +0800

    first commit

---
+++ app/src/main/java/com/example/dogoodsoft_app/lessismore/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dogoodsoft_app.lessismore;
+
+import android.app.Activity;
+
+/**
+ * Created by dogoodsoft-app on 2018/3/26.
+ */
+
+public class MainActivity extends Activity{
+
+
+
+
+
+}
