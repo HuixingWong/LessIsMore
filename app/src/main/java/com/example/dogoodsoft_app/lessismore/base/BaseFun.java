99a31f3caf77cd602dc9e98b8d4c6a9734cd165a
huixing <121674028@qq.com>
2018-04-13 17:23:48 +0800

    commit test

---
+++ app/src/main/java/com/example/dogoodsoft_app/lessismore/base/BaseFun.java
@@ -0,0 +1,18 @@
+package com.example.dogoodsoft_app.lessismore.base;
+
+/**
+ * Created by dogoodsoft-app on 2018/3/28.
+ */
+
+public interface BaseFun {
+
+    void initData();
+
+    void initView();
+
+    void initListener();
+
+    void initLoad();
+
+
+}
